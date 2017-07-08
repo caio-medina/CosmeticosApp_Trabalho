edcaefbc04559055cebb628def8d8a1d09974768
Caio Medina <caio__medina@hotmail.com>
2017-07-08 17:27:04 -0300

    Tela Inicial Pronta

--- app/src/main/java/a2017_1/pooa/iff/br/cosmeticosapp_trabalho/MainActivity.java
+++ app/src/main/java/a2017_1/pooa/iff/br/cosmeticosapp_trabalho/MainActivity.java
@@ -2,12 +2,19 @@ package a2017_1.pooa.iff.br.cosmeticosapp_trabalho;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logo = (ImageView) findViewById(R.id.img_cosmeticos);
+        logo.setImageResource(R.drawable.img_cosmeticos);
     }
 }
