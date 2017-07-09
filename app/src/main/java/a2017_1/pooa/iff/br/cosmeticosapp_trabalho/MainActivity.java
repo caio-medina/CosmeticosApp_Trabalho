8a06e1c77e19f18944a8624972e4dd29f625dbea
Caio Medina <caio__medina@hotmail.com>
2017-07-09 17:27:41 -0300

    Tela de Login

--- app/src/main/java/a2017_1/pooa/iff/br/cosmeticosapp_trabalho/MainActivity.java
+++ app/src/main/java/a2017_1/pooa/iff/br/cosmeticosapp_trabalho/MainActivity.java
@@ -1,8 +1,11 @@
 package a2017_1.pooa.iff.br.cosmeticosapp_trabalho;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +19,21 @@ public class MainActivity extends AppCompatActivity {
 
         logo = (ImageView) findViewById(R.id.img_cosmeticos);
         logo.setImageResource(R.drawable.img_cosmeticos);
+
+
+    }
+
+    public void chamaTelaLogin(View v) {
+        Toast.makeText(this, "Botão funcionando", Toast.LENGTH_SHORT);
+
+        Intent intent = new Intent(MainActivity.this, Login.class);
+        startActivity(intent);
+    }
+
+    public void chamaTelaCadastro(View v) {
+        Toast.makeText(this, "Botão funcionando", Toast.LENGTH_SHORT);
+
+        Intent intent = new Intent(MainActivity.this, Cadastro.class);
+        startActivity(intent);
     }
 }
