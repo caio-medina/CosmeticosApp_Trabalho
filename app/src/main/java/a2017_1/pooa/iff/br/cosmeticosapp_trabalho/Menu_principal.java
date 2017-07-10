5dc715d6446fdfdf1557d04dadab538e52f90880
Caio Medina <caio__medina@hotmail.com>
2017-07-10 13:22:30 -0300

    Telas Carrinho/Corpo/Rosto

--- app/src/main/java/a2017_1/pooa/iff/br/cosmeticosapp_trabalho/Menu_principal.java
+++ app/src/main/java/a2017_1/pooa/iff/br/cosmeticosapp_trabalho/Menu_principal.java
@@ -1,7 +1,10 @@
 package a2017_1.pooa.iff.br.cosmeticosapp_trabalho;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class Menu_principal extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class Menu_principal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+    }
+
+    public void chamaTelaCorpo(View v) {
+        Toast.makeText(this, "Botão funcionando", Toast.LENGTH_SHORT);
+
+        Intent intent = new Intent(Menu_principal.this, Corpo.class);
+        startActivity(intent);
+    }
+
+    public void chamaTelaRosto(View v) {
+        Toast.makeText(this, "Botão funcionando", Toast.LENGTH_SHORT);
+
+        Intent intent = new Intent(Menu_principal.this, Rosto.class);
+        startActivity(intent);
+    }
+
+    public void chamaTelaCarrinho(View v) {
+        Toast.makeText(this, "Botão funcionando", Toast.LENGTH_SHORT);
+
+        Intent intent = new Intent(Menu_principal.this, Carrinho.class);
+        startActivity(intent);
     }
 }
