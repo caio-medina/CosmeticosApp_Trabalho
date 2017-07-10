08e3e5e69107be4897dc44d6e9daf9a891ac70a6
Caio Medina <caio__medina@hotmail.com>
2017-07-10 18:03:29 -0300

    Iteracoes entre telas e design pronto - P1

--- app/src/main/java/a2017_1/pooa/iff/br/cosmeticosapp_trabalho/Corpo.java
+++ app/src/main/java/a2017_1/pooa/iff/br/cosmeticosapp_trabalho/Corpo.java
@@ -1,7 +1,10 @@
 package a2017_1.pooa.iff.br.cosmeticosapp_trabalho;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class Corpo extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class Corpo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_corpo);
+    }
+
+    public void chamaTelaCarrinho(View v) {
+        Toast.makeText(this, "Botão funcionando", Toast.LENGTH_SHORT);
+
+        Intent intent = new Intent(Corpo.this, Carrinho.class);
+        startActivity(intent);
     }
 }
