59137dad610eb39c1ac6b37c70089935bbdd1ce5
Caio Medina <caio__medina@hotmail.com>
2017-07-10 12:37:20 -0300

    Tela do Menu

--- app/src/main/java/a2017_1/pooa/iff/br/cosmeticosapp_trabalho/Login.java
+++ app/src/main/java/a2017_1/pooa/iff/br/cosmeticosapp_trabalho/Login.java
@@ -1,7 +1,10 @@
 package a2017_1.pooa.iff.br.cosmeticosapp_trabalho;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
@@ -10,4 +13,12 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    public void chamaTelaMenu(View v) {
+        Toast.makeText(this, "Botão funcionando", Toast.LENGTH_SHORT);
+
+        Intent intent = new Intent(Login.this, Menu_principal.class);
+        startActivity(intent);
+    }
+
 }
