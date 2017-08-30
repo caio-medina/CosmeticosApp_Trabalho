2decf6cffe990c4ede326749b139b31cd6ed7168
Caio Medina <caio__medina@hotmail.com>
2017-08-30 15:59:06 -0300

    Criando configuração do Realm

--- app/src/main/java/a2017_1/pooa/iff/br/cosmeticosapp_trabalho/activity/CadastroActivity.java
+++ app/src/main/java/a2017_1/pooa/iff/br/cosmeticosapp_trabalho/activity/CadastroActivity.java
@@ -3,19 +3,26 @@ package a2017_1.pooa.iff.br.cosmeticosapp_trabalho.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Toast;
 
 import a2017_1.pooa.iff.br.cosmeticosapp_trabalho.Menu_principal;
 import a2017_1.pooa.iff.br.cosmeticosapp_trabalho.R;
 import a2017_1.pooa.iff.br.cosmeticosapp_trabalho.adapter.ClickRecyclerViewListener;
+import io.realm.Realm;
 
 public class CadastroActivity extends AppCompatActivity implements ClickRecyclerViewListener {
 
+    private Realm realm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        realm = Realm.getDefaultInstance();
+
+
     }
 
     public void chamaTelaMenu(View v) {
---
+++ app/src/main/java/a2017_1/pooa/iff/br/cosmeticosapp_trabalho/persistence/BDConfig.java
@@ -0,0 +1,23 @@
+package a2017_1.pooa.iff.br.cosmeticosapp_trabalho.persistence;
+
+import android.app.Application;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+
+public class BDConfig  extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Realm.init(getApplicationContext());
+
+        RealmConfiguration realmConfiguration = new RealmConfiguration.Builder()
+                .name(Realm.DEFAULT_REALM_NAME)
+                .schemaVersion(0)
+                .deleteRealmIfMigrationNeeded()
+                .build();
+        Realm.setDefaultConfiguration(realmConfiguration);
+    }
+}
